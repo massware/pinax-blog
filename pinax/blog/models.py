ff50f66dad288322f9fff55406f9373af3f31ea4
josemoya19 <59899505+josemoya19@users.noreply.github.com>
2020-04-17 17:31:04 -0500

    Update models.py
    support for python3+ and django3+

--- pinax/blog/models.py
+++ pinax/blog/models.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.html import strip_tags
 from django.utils.translation import ugettext_lazy as _
 
